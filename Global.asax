a208249a291be37239ad0c9f4a22f1b7393711c7
Gurlinkaur23 <gurlinkaur338@gmail.com>
2024-03-11 22:05:33 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Integrated_Product_Management.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Integrated_Product_Management.Startup))]
+namespace Integrated_Product_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
